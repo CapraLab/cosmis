81e6ac1c85a253016ca2d16a9942d3ead72dd4ba
Bian Li <comput.biol.geek@gmail.com>
2020-09-28 10:23:19 -0500

    Added search_for_all_contacts to pdb_utils, added get_codon_muation_rates to seq_utils, added a utility script retrieve_features.py for computing contact set features

--- cosmis/utils/seq_utils.py
+++ cosmis/utils/seq_utils.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from mtr3d.utils.genetic_code import GENETIC_CODE
+from cosmis.utils.genetic_code import GENETIC_CODE
 
 def place_holder():
     """
@@ -70,3 +70,86 @@ def count_cds_ns(cds):
     for i in range(0, len(cds), 3):
         cds_ns.append(count_codon_ns(cds[i:i+3]))
     return cds_ns
+
+
+def get_codon_mutation_rates(cds):
+    """
+
+    Parameters
+    ----------
+    cds : str
+        Coding sequence.
+
+    Returns
+    -------
+    mutation probabilities : list
+        A list of tuples consisting of synonymous and nonsynonymous mutation
+        probabilities.
+    """
+    if len(cds) % 3 != 0:
+        raise ValueError('Given CDS length is not a multiple of 3.')
+
+    num_codons = len(cds) // 3
+
+    mutation_rates = []
+    # one nucleotide before and one nucleotide after the codon
+    for codon_number in range(1, num_codons + 1):
+        # determine the codon sequence
+        codon_sequence = cds[(codon_number - 1) * 3:codon_number * 3]
+
+        synonymous_rate = 0
+        nonsynonymous_rate = 0
+
+        # determine the mutation rate of the first and the last codons
+        # consider only two mutatable nucleotides
+        if codon_number == 1 or codon_number == num_codons:
+            # first codon
+            if codon_number == 1:
+                sequence_context = cds[:4]
+                # i is the zero-indexed position of the mutated nucleotide
+                for i in range(1, 3):
+                    trinucleotide = sequence_context[i - 1:i + 2]
+                    rates = MUTATION_RATES_UNIQUE[trinucleotide]
+                    for k, v in rates.items():
+                        mutant_sequence = codon_sequence[:i] + k[1] + codon_sequence[i + 1:]
+                        if GENETIC_CODE[codon_sequence] == GENETIC_CODE[mutant_sequence]:
+                            synonymous_rate += v
+                        elif GENETIC_CODE[codon_sequence] != GENETIC_CODE[mutant_sequence] \
+                            and GENETIC_CODE[mutant_sequence] != 'STOP':
+                            nonsynonymous_rate += v
+            # last codon
+            else:
+                sequence_context = cds[-4:]
+                # i is the zero-indexed position of the mutated nucleotide
+                for i in range(0, 2):
+                    trinucleotide = sequence_context[i:i + 3]
+                    rates = MUTATION_RATES_UNIQUE[trinucleotide]
+                    for k, v in rates.items():
+                        mutant_sequence = codon_sequence[:i] + k[1] + codon_sequence[i + 1:]
+                        if GENETIC_CODE[codon_sequence] == GENETIC_CODE[mutant_sequence]:
+                            synonymous_rate += v
+                        elif GENETIC_CODE[codon_sequence] != GENETIC_CODE[mutant_sequence] \
+                            and GENETIC_CODE[mutant_sequence] != 'STOP':
+                            nonsynonymous_rate += v
+
+        # codons other than the first and the last
+        # consider all three mutatable nucleotides
+        else:
+            # one nucleotide before and one nucleotide after the codon
+            sequence_context = cds[(codon_number - 1) * 3 - 1:(codon_number - 1) * 3 + 4]
+            # mutate nucleotide in the codon iteratively
+            for i in range(3):
+                trinucleotide = sequence_context[i:i + 3]
+                rates = MUTATION_RATES_UNIQUE[trinucleotide]
+                for k, v in rates.items():
+                    codon_seq_list = list(codon_sequence)
+                    codon_seq_list[i] = k[1]
+                    mutant_sequence = ''.join(codon_seq_list)
+                    if GENETIC_CODE[codon_sequence] == GENETIC_CODE[mutant_sequence]:
+                        synonymous_rate += v
+                    elif GENETIC_CODE[codon_sequence] != GENETIC_CODE[mutant_sequence] \
+                        and GENETIC_CODE[mutant_sequence] != 'STOP':
+                        nonsynonymous_rate += v
+
+        mutation_rates.append((synonymous_rate, nonsynonymous_rate))
+    return mutation_rates
